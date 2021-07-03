9de730a69456c4093197de3df6147146d8ae095c
Vinay Haryan <65211111+VinayHaryan@users.noreply.github.com>
2021-07-03 11:48:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request
 import pickle
 import numpy as np
 
-# Load the Random Forest CLassifier model
+
 filename = 'first-innings-score-lr-model.pkl'
 regressor = pickle.load(open(filename, 'rb'))
 
